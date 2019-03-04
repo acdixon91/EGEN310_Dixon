9e2904e5c251dbfc162dee7b13c0afc465cc31fb
Andrew Dixon <acdixon91@gmail.com>
2019-03-03 21:40:51 -0700

    Initial Commit

---
+++ ArduinoBluetooth/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArduinoBluetooth
+//
+//  Created by Andrew Dixon on 3/3/19.
+//  Copyright © 2019 Andrew Dixon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArduinoBluetooth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArduinoBluetooth
+//
+//  Created by Andrew Dixon on 3/3/19.
+//  Copyright © 2019 Andrew Dixon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
