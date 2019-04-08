093a08e20534d2f2a246007ed0c19a7cf90a1824
Andrew Dixon <acdixon91@gmail.com>
2019-04-08 14:50:46 -0600

    changed comments

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,16 +1,3 @@
-/*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
 
 #include <Arduino.h>
 #include <SPI.h>
